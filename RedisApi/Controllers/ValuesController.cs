a2d878637b58757be93ca0a31ef44016526b9417
Michael Li <lihb12366@gmail.com>
2022-04-06 01:53:21 -0400

    push to github

---
+++ RedisApi/Controllers/ValuesController.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        private string _containerName = "data";
+        private readonly IDistributedCache distributedCache;
+        public ValuesController(IConfiguration configuration, IDistributedCache distributedCache)
+        {
+            this._configuration = configuration;
+            this.distributedCache = distributedCache;
+        }
+        public string getConnection()
+        {
+            var _connectionstring = _configuration.GetConnectionString("storageConnection");
+            return _connectionstring;
+
+        }
+        private string GetFromStorageAsync()
+        {
+            string storageAccount_connection = "";
+            BlobServiceClient blobServiceClient = new BlobServiceClient(getConnection());
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+            BlobClient blobClient = containerClient.GetBlobClient("FileA.txt");
+            MemoryStream memoryStream = new MemoryStream();
+            blobClient.DownloadTo(memoryStream);
+            memoryStream.Position = 0;
+            string content = new StreamReader(memoryStream).ReadToEnd();
+            return content;
+        }
+        
+        public async Task<string> Getvalue()
+        {
+            var valueFromCache = await distributedCache.GetStringAsync("key_1");
+            if (!string.IsNullOrEmpty(valueFromCache))
+            {
+                return valueFromCache;
+            }
+            else
+            {
+                string valueFromStorage = GetFromStorageAsync();
+                await distributedCache.SetStringAsync("key_1", valueFromStorage);
+                return valueFromStorage;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = Getvalue();
+            BlobServiceClient blobServiceClient = new BlobServiceClient(getConnection());
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+            BlobClient blobClient = containerClient.GetBlobClient("FileA.txt");
+            MemoryStream memoryStream = new MemoryStream();
+            blobClient.DownloadTo(memoryStream);
+            memoryStream.Position = 0;
+            string content = new StreamReader(memoryStream).ReadToEnd();
+            return Ok();
+        }
+
+    }
+}
---
+++ RedisApi/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddDistributedRedisCache(options=>
+            {
+                options.Configuration = "RedisTestMli.redis.cache.windows.net:6380,password=yExgO0KIQ31ljJJAyiKEjnlrN6aT6fhKPAzCaMH5VZQ=,ssl=True,abortConnect=False";
+                options.InstanceName = "master";
+            }
+                
+                );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
